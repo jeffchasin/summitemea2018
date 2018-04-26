62988f83ff73b1a1b5f62e376fd98cf56a960f8b
Jeff Chasin <chasin@adobe.com>
2018-04-26 10:34:46 -0400

    after-lib

--- extension-html-pattern/src/lib/actions/loadHotjarCode.js
+++ extension-html-pattern/src/lib/actions/loadHotjarCode.js
@@ -1,5 +1,15 @@
 'use strict';
 
-module.exports = function(settings) {
-  // TODO Perform some action.
+var extensionSettings = turbine.getExtensionSettings();
+
+module.exports = function (settings) {
+  // TODO Perform some action
+  (function (h, o, t, j, a, r) {
+    h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
+    h._hjSettings = { hjid: extensionSettings.hjid, hjsv: 6 };
+    a = o.getElementsByTagName('head')[0];
+    r = o.createElement('script'); r.async = 1;
+    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+    a.appendChild(r);
+  })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
 };
